e2acfd59af688b99684b581e6d7442d773a9c19d
ventsy95 <ventsy95@mail.bg>
2016-07-21 20:28:21 +0300

    Removed 2 unnecessary constants from AllWork. FreePlacesForTasks and currentUnassignedTask's access changed to package, fixed methods to work properly with them. Removed unnecessary getters and setters.

--- AllWork.java
+++ AllWork.java
@@ -1,59 +1,42 @@
 
 public class AllWork {
-	private static final int RIGHT_END_OF_OPEN_INTERVAL_FOR_FREE_PLACES = 11;
-	private static final int LEFT_END_OF_OPEN_INTERVAL_FOR_FREE_PLACES = -1;
 	private static final int NUMBER_OF_TASKS = 10;
 	Task[] tasks;
-	private int freePlacesForTasks;
-	private int currentUnassignedTask;
+	int freePlacesForTasks;
+	int currentUnassignedTask;
 
 	AllWork() {
 		this.freePlacesForTasks = NUMBER_OF_TASKS;
 		this.tasks = new Task[freePlacesForTasks];
-		this.currentUnassignedTask=0;
+		this.currentUnassignedTask = 0;
 	}
-	
-	public void addTask(Task task){
-		if(this.tasks.length-freePlacesForTasks<NUMBER_OF_TASKS){
-			this.tasks[this.tasks.length-freePlacesForTasks]=task;
-			this.setFreePlacesForTasks(this.freePlacesForTasks-1);
+
+	public void addTask(Task task) {
+		if (this.tasks.length - freePlacesForTasks < NUMBER_OF_TASKS) {
+			this.tasks[this.tasks.length - freePlacesForTasks] = task;
+			this.freePlacesForTasks -= 1;
 		}
 	}
-	
-	public Task getNextTask(){
-		this.setCurrentUnassignedTask(this.getCurrentUnassignedTask()+1);
-		return this.tasks[this.getCurrentUnassignedTask()-1];
+
+	public Task getNextTask() {
+		if (this.currentUnassignedTask == this.tasks.length - 1) {
+			return this.tasks[this.currentUnassignedTask];
+		}
+		if (this.currentUnassignedTask < this.tasks.length - 1) {
+			this.currentUnassignedTask += 1;
+			return this.tasks[this.currentUnassignedTask - 1];
+		} else {
+			System.out.println("No tasks");
+			return null;
+		}
 	}
-	
-	public boolean isAllWorkDone(){
-		for(int index=0; index<this.tasks.length; index++){
-			if(this.tasks[index].getWorkingHours()!=0){
+
+	public boolean isAllWorkDone() {
+		for (int index = 0; index < this.tasks.length; index++) {
+			if (this.tasks[index].getWorkingHours() != 0) {
 				return false;
 			}
 		}
 		return true;
 	}
-	
-	
-
-	public int getFreePlacesForTasks() {
-		return freePlacesForTasks;
-	}
-
-	public void setFreePlacesForTasks(int freePlacesForTasks) {
-		if (freePlacesForTasks > LEFT_END_OF_OPEN_INTERVAL_FOR_FREE_PLACES && freePlacesForTasks < RIGHT_END_OF_OPEN_INTERVAL_FOR_FREE_PLACES) {
-			this.freePlacesForTasks = freePlacesForTasks;
-		}
-	}
-
-	public int getCurrentUnassignedTask() {
-		return currentUnassignedTask;
-	}
-
-	public void setCurrentUnassignedTask(int currentUnassignedTask) {
-		if (currentUnassignedTask > LEFT_END_OF_OPEN_INTERVAL_FOR_FREE_PLACES && currentUnassignedTask < RIGHT_END_OF_OPEN_INTERVAL_FOR_FREE_PLACES) {
-			this.currentUnassignedTask = currentUnassignedTask;
-		}
-	}
-
 }
